a277b88de8bd8abb76260841648da42d8ae0aa27
Oksi598 <radovunoksana@gmail.com>
2025-06-16 22:49:01 +0300

    identity

---
+++ pinterest_back/PinterestClone.DAL/Models/BoardPin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinterestClone.DAL.Models
+{
+    public class BoardPin
+    {
+        public Guid BoardId { get; set; }
+        public Board Board { get; set; } = null!;
+
+        public Guid PinId { get; set; }
+        public Pin Pin { get; set; } = null!;
+    }
+
+}
---
+++ pinterest_back/PinterestClone.DAL/Models/Identity/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace PinterestClone.DAL.Models.Identity
+{
+    public class User : IdentityUser<string>
+    {
+        public string DisplayName { get; set; } = string.Empty;
+        public string? AvatarUrl { get; set; }
+        public string? Bio { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+        public string? Gender { get; set; }
+        public string? Country { get; set; }
+        public string? Language { get; set; }
+
+        public bool IsProfilePublic { get; set; } = true;
+
+        public virtual ICollection<Board> Boards { get; set; } = [];
+        public virtual ICollection<Pin> Pins { get; set; } = [];
+        public virtual ICollection<Comment> Comments { get; set; } = [];
+        public virtual ICollection<Like> Likes { get; set; } = [];
+    }
+}
