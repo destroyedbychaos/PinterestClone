95af009b674ee520e0a75eee321fa4090d75399a
maxim <167625699+RiXeN12@users.noreply.github.com>
2025-07-19 21:04:17 +0300

    add SideMenu

--- pinterest_front/src/components/layout/Layout.jsx
+++ pinterest_front/src/components/layout/Layout.jsx
@@ -2,17 +2,20 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import SideMenu from "./SideMenu";
 
 const Layout = memo(() => {
     return (
         <div className="flex flex-col min-h-screen">
-            <Header />
-
-            <main>
-                <div>
-                    <Outlet />
-                </div>
-            </main>
+             <div className="flex flex-1">
+                <SideMenu />
+                
+                <main className="flex-1">
+                    <div>
+                        <Outlet />
+                    </div>
+                </main>
+            </div>
 
             <Footer />
         </div>
