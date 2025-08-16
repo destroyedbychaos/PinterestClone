5c97f651f8b47dbd8b58501ade9e302e303d9e69
maxim <167625699+RiXeN12@users.noreply.github.com>
2025-08-16 16:31:51 +0300

    front

---
+++ pinterest_front/src/components/data/vibes.js
@@ -0,0 +1,53 @@
+
+export const vibes = [
+    {
+        id: '1',
+        image: '../../../src/assets/images/vibes/1.png',
+        height: 290,
+    },
+    {
+        id: '2',
+        image: '../../../src/assets/images/vibes/2.png',
+        height: 180,
+    },
+    {
+        id: '3',
+        image: '../../../src/assets/images/vibes/3.png',
+        height: 290,
+    },
+    {
+        id: '4',
+        image: '../../../src/assets/images/vibes/4.png',
+        height: 400,
+    },
+    {
+        id: '5',
+        image: '../../../src/assets/images/vibes/5.png',
+        height: 400,
+    },
+    {
+        id: '6',
+        image: '../../../src/assets/images/vibes/6.png',
+        height: 400,
+    },
+    {
+        id: '7',
+        image: '../../../src/assets/images/vibes/7.png',
+        height: 180,
+    },
+    {
+        id: '8',
+        image: '../../../src/assets/images/vibes/8.png',
+        height: 290,
+    },
+    {
+        id: '9',
+        image: '../../../src/assets/images/vibes/9.png',
+        height: 180,
+    },
+    {
+        id: '10',
+        image: '../../../src/assets/images/vibes/10.png',
+        height: 290,
+    },
+];
