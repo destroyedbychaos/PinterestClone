3cc62e722cdaac1c254e3c9c65f9f0b7b040e4f3
Dima1385 <Lozik1385@gmail.com>
2025-08-20 04:11:43 +0300

    fix after merge

--- pinterest_front/src/routes/BasicRoute.jsx
+++ pinterest_front/src/routes/BasicRoute.jsx
@@ -4,6 +4,7 @@ import NotFoundPage from "../components/NotFoundPage";
 import HomePage from "../pages/Home/HomePage";
 import Layout from "../components/layout/Layout";
 import LayoutWithoutSideMenu from "../components/layout/LayoutWithoutSideMenu";
+import LayoutWithoutFooter from "../components/layout/LayoutWithoutFooter";
 import LoginForm from "../pages/Auth/LoginForm.jsx";
 import RegisterForm from "../pages/Auth/RegisterForm.jsx";
 import ForgotPassword from "../pages/ForgotPassword/ForgotPassword.jsx";
