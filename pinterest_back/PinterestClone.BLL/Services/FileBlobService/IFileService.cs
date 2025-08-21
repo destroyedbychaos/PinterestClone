b1cc61e4ff375040dbbf38717cfa4fef342bdddf
Yeva <zhezhuk.eva@gmail.com>
2025-08-21 00:41:02 -0400

    API Blob Structure Add - Added BlobDto and BlobResponseDto to describe blobs - Added IFileService and FileService - Added BlobController to access blobs from user side

---
+++ pinterest_back/PinterestClone.BLL/Services/FileBlobService/IFileService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using PinterestClone.BLL.DTOs;
+
+namespace PinterestClone.BLL.Services.FileBlobService
+{
+    public interface IFileService
+    {
+        Task<List<BlobDto?>> GetAllBlobsAsync();
+        Task<BlobResponseDto> UploadAsync(IFormFile blob);
+        Task<BlobDto> DownloadAsync(string blobFilename);
+        Task<BlobResponseDto> DeleteAsync(string blobFilename);
+    }
+}
+
