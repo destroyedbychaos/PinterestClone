d3a17f4a840c61ca043be8f857b8b8084cff6140
Oksi598 <radovunoksana@gmail.com>
2025-09-03 23:08:02 +0300

    notification header

--- pinterest_front/src/routes/BasicRoute.jsx
+++ pinterest_front/src/routes/BasicRoute.jsx
@@ -16,6 +16,7 @@ import ProfileEdit from "../pages/Profile/ProfileEdit.jsx";
 import SearchFilter from "../pages/Search/SearchFilter.jsx";
 import SearchProfile from "../pages/Search/SearchProfile.jsx";
 import UserProfile from "../pages/Profile/UserProfile.jsx";
+import Notifications from "../pages/Notifications/Notifications.jsx";
 
 
 
@@ -63,7 +64,9 @@ const BasicRoute = () => {
             <Route path="/search-profile" element={<Layout />}>
                 <Route index element={<SearchProfile/>} />
             </Route>
-            
+            <Route path="/notifications" element={<Layout />}>
+                <Route index element={<Notifications/>} />
+            </Route>
 
             <Route path="/user/:username" element={<LayoutWithoutFooter />}>
                 <Route index element={<UserProfile/>} />
